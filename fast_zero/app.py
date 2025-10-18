d336a23c6fcbb1a8de2139a0339759d23dea87fe
virgilio-vpr <62821248+virgilio-vpr@users.noreply.github.com>
2025-10-18 18:30:42 +0000

    Segunda aula

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from .schemas import Message
+
+app = FastAPI(title='Fast Zero', version='0.1.0')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'Message': 'Olá Mundo!'}
+
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+        <head>
+            <title>Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1> Olá Mundo </h1>
+        </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,4 +11,13 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.json() == {'Message': 'Olá Mundo!'}
+
+
+def test_exercicio_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/exercicio-html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
