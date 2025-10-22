4f10bb2d05359704bdbd39e62716a7ee3ada050f
virgilio-vpr <62821248+virgilio-vpr@users.noreply.github.com>
2025-10-22 15:47:21 +0000

    Implementando rotas de CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,26 +1,54 @@
 from http import HTTPStatus
+from http.client import HTTPException
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
-from .schemas import Message
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI(title='Fast Zero', version='0.1.0')
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'Message': 'Olá Mundo!'}
+    return {'message': 'Olá Mundo!'}
 
 
-@app.get('/exercicio-html', response_class=HTMLResponse)
-def exercicio_aula_02():
-    return """
-    <html>
-        <head>
-            <title>Nosso olá mundo!</title>
-        </head>
-        <body>
-            <h1> Olá Mundo </h1>
-        </body>
-    </html>"""
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
