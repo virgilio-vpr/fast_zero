3ce4d75842ffa9329a02960fb5ec34a9c454edf4
virgilio-vpr <62821248+virgilio-vpr@users.noreply.github.com>
2025-10-24 16:55:50 +0000

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
-from http.client import HTTPException
 
-from fastapi import FastAPI
+# from http.client import HTTPException
+from fastapi import FastAPI, HTTPException
 
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
@@ -32,9 +32,7 @@ def read_users():
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        )
+        raise HTTPException(status_code=404, detail='User not found')
 
     user_with_id = UserDB(**user.model_dump(), id=user_id)
     database[user_id - 1] = user_with_id
@@ -52,3 +50,11 @@ def delete_user(user_id: int):
     del database[user_id - 1]
 
     return {'message': 'User deleted'}
+
+
+# @app.get('/users/{user_id}', response_model=UserPublic)
+# def read_user__exercicio(user_id: int):
+#     if user_id > len(database) or user_id < 1:
+#         raise HTTPException(status_code=404, detail='User not found')
+
+#     return database[user_id - 1]
--- tests/test_app.py
+++ tests/test_app.py
@@ -61,3 +61,34 @@ def test_delete_user(client):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+# def test_update_user_should_return_not_found__exercicio(client):
+#     response = client.put(
+#         '/users/666',
+#         json={
+#             'username': 'bob',
+#             'email': 'bob@example.com',
+#             'password': 'mynewpassword',
+#         },
+#     )
+#     assert response.status_code == HTTPStatus.NOT_FOUND
+#     assert response.json() == {'detail': 'User not found'}
+
+
+# def test_delete_user_should_return_not_found__exercicio(client):
+#     response = client.delete('/users/666')
+
+#     assert response.status_code == HTTPStatus.NOT_FOUND
+#     assert response.json() == {'detail': 'User not found'}
+
+
+# def test_get_user___exercicio(client):
+#     response = client.get('/users/1')
+
+#     assert response.status_code == HTTPStatus.OK
+#     assert response.json() == {
+#         'username': 'bob',
+#         'email': 'bob@example.com',
+#         'id': 1,
+#     }
